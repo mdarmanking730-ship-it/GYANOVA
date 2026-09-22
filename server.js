b13ac52a2a8fef7982e73b7cc7876f03721a3db9
mdarmanking730-ship-it <mdarmanking730@gmail.com>
2026-09-22 14:34:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "GYANOVA API is running 🚀"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`GYANOVA running on port ${PORT}`);
+});
